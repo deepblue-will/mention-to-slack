1aaa62d1b9b60afe5be85e84d60c10e1b45e04fa
Aoshi <deepblue.will@gmail.com>
2018-08-01 13:58:51 +0900

    :heavy_plus_sign: add prettier

--- slack_name.js
+++ slack_name.js
@@ -1,7 +1,7 @@
 const team = require('./team.json');
 
 module.exports = {
-  fromGithubName: (githubName) => {
-    return Object.keys(team).find((key) => team[key].github === githubName);
-  }
+  fromGithubName: githubName => {
+    return Object.keys(team).find(key => team[key].github === githubName);
+  },
 };
--- to_slack.js
+++ to_slack.js
@@ -9,7 +9,10 @@ module.exports = async () => {
   const conversationId = 'C14G28YV8';
   const name = slackName.fromGithubName('@deepblue-will');
   try {
-    await web.chat.postMessage({ channel: conversationId, text: `${name} aaaa` });
+    await web.chat.postMessage({
+      channel: conversationId,
+      text: `${name} aaaa`,
+    });
   } catch (error) {
     console.error(error);
   }
