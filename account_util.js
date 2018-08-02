86199ee2206924ece20fc656031940ded3aba226
Aoshi <deepblue.will@gmail.com>
2018-08-02 11:18:16 +0900

    :+1: replace github mention to slack mention

---
+++ account_util.js
@@ -0,0 +1,21 @@
+const ACCOUNTS = require('./accounts');
+
+const MENTION_REGEXP = /(?:^|[^a-zA-Z0-9_＠!@#$%&*])(?:(?:@|＠)(?!\/))([a-zA-Z0-9/_.-]+)(?:\b(?!@|＠)|$)/g;
+
+class AccountUtil {
+  static getSlackNameByGitHub(name) {
+    return AccountUtil._getSlackName('github', name);
+  }
+
+  static getMentions(text) {
+    const matches = text.match(MENTION_REGEXP);
+    return matches ? matches.map((str) => str.trim()): null;
+  }
+
+  static _getSlackName(serviceName, name) {
+    const account = ACCOUNTS.find(account => account[serviceName] === name);
+    return account ? account.slack : null;
+  }
+}
+
+module.exports = AccountUtil;
--- create_slack_message.js
+++ create_slack_message.js
@@ -1,3 +1,5 @@
+const AccountUtil = require('./account_util');
+
 const createMessageFromGithub = ({
   action,
   issue,
@@ -9,6 +11,20 @@ const createMessageFromGithub = ({
     return null;
   }
 
+  const mentions = AccountUtil.getMentions(comment.body);
+  if (!mentions) {
+    return null;
+  }
+
+  const text = mentions.reduce((str, mention) => {
+    const slackName = AccountUtil.getSlackNameByGitHub(mention);
+    if (slackName) {
+      return str.replace(new RegExp(mention, 'g'), slackName);
+    } else {
+      return str;
+    }
+  }, comment.body);
+
   //see: https://api.slack.com/docs/message-attachments
   return {
     color: '#333',
@@ -18,7 +34,7 @@ const createMessageFromGithub = ({
     author_icon: sender.avatar_url,
     title: `${issue.title} #${issue.number}`,
     title_link: comment.html_url,
-    text: comment.body,
+    text,
     footer: repository.full_name,
     ts: new Date(comment.updated_at).getTime() / 1000,
   };
